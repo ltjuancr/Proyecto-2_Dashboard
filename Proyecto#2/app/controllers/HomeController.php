7e8e5ba6c53e93f1e1acf5d862c6493b17b315ee
Juan Quiros Gonzalez <juan_gonzalez93@hotmail.com>
2014-11-27 11:29:31 -0600

    se inicia drag and drop para las tareas

--- Proyecto#2/app/controllers/HomeController.php
+++ Proyecto#2/app/controllers/HomeController.php
@@ -37,5 +37,104 @@ class HomeController extends BaseController {
 		//$this->layout->nest('content', 'tareas1');
 	}
 
+    public function tareasAjax()
+	{
+       $id = Auth::user()->id;
+       $tareas = Tarea::tareasDisponibles($id);
+	   //var_dump($tareas);
+      
+       return Response::Json($tareas);
+	}
+
+
+		public function destroy($id)
+	{
+		$tarea = Tarea::find($id);
+		$tarea->delete();
+		return Redirect::to('tareas');
+	}
+
+	public function create()
+	{
+		$this->layout->titulo = 'Crear Tarea';
+		$this->layout->nest(
+			'content',
+			'tareas.create',
+			array()
+		);
+	}
+
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		$titulo = Input::get('titulo');
+		$prioridad= Input::get('prioridad');
+		$descripcion = Input::get('descripcion');
+		$estado = 'nueva';
+		$id_user = Auth::user()->id;
+
+		$tarea = new Tarea();
+		$tarea->titulo = $titulo;
+		$tarea->prioridad = $prioridad;
+		$tarea->descripcion = $descripcion;
+		$tarea->estado = $estado;
+		$tarea->id_user = $id_user;
+		$tarea->save();
+		return Redirect::to('tareas');
+	}
+
+	public function cambiarEstado()
+	{
+		$id = Input::get('id');
+		$estado = Input::get('estado');
+
+
+        $tarea = Tarea::find($id);
+		$tarea->estado = $estado;
+		$tarea->save();
+		return Response::Json("hola");
+	}
+
+		public function edit($id)
+	{
+		$this->layout->titulo = 'Editar Tarea';
+		$tarea = Tarea::find($id);
+		$this->layout->nest(
+			'content',
+			'tareas.edit',
+			array(
+				'tarea' => $tarea
+			)
+		);
+	}
+
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		$titulo = Input::get('titulo');
+		$prioridad= Input::get('prioridad');
+		$descripcion = Input::get('descripcion');
+
+		$tarea = Tarea::find($id);
+		$tarea->titulo = $titulo;
+		$tarea->prioridad = $prioridad;
+		$tarea->descripcion = $descripcion;
+		$tarea->save();
+
+		return Redirect::to('tareas');
+
+	}
+
 
 }
